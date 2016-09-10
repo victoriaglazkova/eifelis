710b21dc756eeab13944e95ff2cc9d2e58785a2f
Edgars <dev2.shepchem@gmail.com>
2016-09-10 21:22:46 +0300

    test

---
+++ index.php
@@ -0,0 +1,35 @@
+{
+  "project_info": {
+    "project_number": "598659596228",
+    "project_id": "bindie-bb7ce"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:598659596228:android:f1d156b6e2368ee2",
+        "android_client_info": {
+          "package_name": "com.bindie.app.bindie"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "AIzaSyB3aW1lJEJfYQcZkexOBrEink3VKwOIlwY"
+        }
+      ],
+      "services": {
+        "analytics_service": {
+          "status": 1
+        },
+        "appinvite_service": {
+          "status": 1,
+          "other_platform_oauth_client": []
+        },
+        "ads_service": {
+          "status": 1
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
